8e5b4d3391c8e996362e5fc2df405e22dfe425ff
idraskovits <i.draskovits@ncl.ac.uk>
2019-10-28 00:52:30 +0000

    First commit 🤠

---
+++ node/src/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const fs = require('fs');
+
+
+app.use(cors());
+app.use(express.urlencoded());
+// might not be needed
+app.use(express.json());
+
+
+//////////////
+
+app.post('/save', (req, res) => {
+    let filename = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwqyz0123456789';
+    for(let i = 0; i < 5; i++) {
+        filename += possible.charAt(Math.floor(Math.random() * possible.length));
+    }   
+
+    fs.writeFile("./notes/" + filename + ".txt", req.body.text, function(err) {
+    
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Was cool bruh");
+    });
+    
+    return res.send('Received a POST HTTP method');
+
+});
+
+///////////////
+
+app.get('/read/:filename', (req, res) => {
+    console.log(req.params);
+    
+    fs.readFile('./notes/' + req.params.filename + '.txt', 'utf8', (err, contents) => {
+        console.log(contents);
+        return res.send(contents);
+    }); 
+});
+
+
+// just prints if it worked
+app.listen(3000, () => {
+    console.log("Works");
+});
